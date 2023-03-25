fa167c5af8380410ee3d88eef714f851d5c1e205
melih022 <110200865+melih022@users.noreply.github.com>
2023-03-25 05:25:35 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -165,7 +165,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", None)
+START_IMG_URL = getenv("START_IMG_URL", "startvideo.jpeg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
