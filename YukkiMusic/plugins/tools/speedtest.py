145afb8ef66628a0ccf7dd7f8d3a4f045d2821f8
melih022 <110200865+melih022@users.noreply.github.com>
2023-03-25 05:45:18 +0300

    Update speedtest.py

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -22,13 +22,13 @@ def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("Running Download SpeedTest")
+        m = m.edit("İndirme Hız Testi Çalıştırılıyor")
         test.download()
-        m = m.edit("Running Upload SpeedTest")
+        m = m.edit("Yükleme Hız Testini Çalıştırılıyor")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("Sharing SpeedTest Results")
+        m = m.edit("Hız Sonuçlarını Paylaşmak İçin Hazır")
     except Exception as e:
         return m.edit(e)
     return result
@@ -36,20 +36,20 @@ def testspeed(m):
 
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("Running Speed test")
+    m = await message.reply_text("Hız Testi")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""**Speedtest Results**
+    output = f"""**<<<<<<Sonuçlar>>>>**
     
 <u>**Client:**</u>
 **__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
+**__Ülke:__** {result['client']['country']}
   
 <u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
+**__İsim:__** {result['server']['name']}
+**__Ülke:__** {result['server']['country']}, {result['server']['cc']}
 **__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
+**__Geçikme:__** {result['server']['latency']}  
 **__Ping:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, 
