d6f03820dafb97c549df4bcc366aee6ba291dc57
intikamailesi <99623476+intikamailesi@users.noreply.github.com>
2023-04-17 00:13:59 +0300

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -32,7 +32,7 @@ def PlayWrapper(command):
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
                 await message.reply_text(
-                    "**Özel Müzik Botu**\n\nBu Botu Kullanabilmeniz İçin Chat /id yazarak Grup İd bizi @Atomfast Veya @MissSahip PM atarak izin vermesini İsteyin.."
+                    "**Üçretli Müzik Botu**\n\nBu Botu Kullanabilmeniz İçin Chat'e /id yazarak Grup İd'sini bize @İntikamKurucusu Veya @AtomFast PM atarak izin vermesini İsteyin.."
                 )
                 return await app.leave_chat(message.chat.id)
         if await is_commanddelete_on(message.chat.id):
