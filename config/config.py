4ec14db9fca4c2eccb87e71da1b55e28a404f10a
intikamailesi <99623476+intikamailesi@users.noreply.github.com>
2023-04-16 23:09:27 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/melih022/videomuzik",
+    "https://github.com/intikamailesi/Videomusicpro",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
